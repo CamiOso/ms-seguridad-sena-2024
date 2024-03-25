22ec156ca8fce7ce22b9f15e2433d3049379fc6a
Cristian Camilo Osorio Granada <crcosoriogr@unal.edu.co>
2024-03-25 07:10:42 -0500

    Se finaliza el entorno del inicio de sesion con el 2fa generando el token y almacenando en base de datos

--- src/controllers/usuario.controller.ts
+++ src/controllers/usuario.controller.ts
@@ -205,6 +205,7 @@ export class UsuarioController {
       login.token = '';
       login.estadoToken = false;
       this.repositorioLogin.create(login);
+      usuario.clave = '';
 
       //notificar al usuario via correo o sms
       return usuario;
@@ -231,10 +232,24 @@ export class UsuarioController {
       let token = this.servicioSeguridad.crearToken(usuario);
       if (usuario) {
         usuario.clave = '';
+        try {
+          this.usuarioRepository.logins(usuario._id).patch(
+            {
+              estadoCodigo2fa: true,
+              token:token
+            },
+            {
+              estadoCodigo2fa: false,
+            },
+          );
+        } catch {
+          console.log(
+            'No se ha almacenado el cambio del estado del token en la base de datos',
+          );
+        }
         return {
-          user:
-            usuario,
-          
+          user: usuario,
+
           token: token,
         };
       }
