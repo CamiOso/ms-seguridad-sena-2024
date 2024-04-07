b4346da1c496c3329e249978c680e8d02f013898
Cristian Camilo Osorio Granada <crcosoriogr@unal.edu.co>
2024-04-07 06:22:37 -0500

    Se agrega un servicio adicional para tener compartida la funcionalidad de consulta de permisos. Se genera una nueva acción expuesta en el controlador de usuario para que sea consumida por otros microservicios

--- src/auth/strategy.ts
+++ src/auth/strategy.ts
@@ -1,84 +1,46 @@
 import {AuthenticationBindings, AuthenticationMetadata, AuthenticationStrategy} from '@loopback/authentication';
+import {inject, service} from '@loopback/core';
+import {repository} from '@loopback/repository';
 import {HttpErrors, Request} from '@loopback/rest';
 import {UserProfile} from '@loopback/security';
 import parseBearerToken from 'parse-bearer-token';
-import {inject, injectable, service} from '@loopback/core';
-import {SeguridadUsuarioService} from '../services';
-import {repository} from '@loopback/repository';
 import {RolMenuRepository} from '../repositories';
-
-@injectable()
+import {AuthService, SeguridadUsuarioService} from '../services';
 export class AuthStrategy implements AuthenticationStrategy {
   name: string = 'auth';
 
   constructor(
     @service(SeguridadUsuarioService)
     private servicioSeguridad: SeguridadUsuarioService,
+    @inject(AuthenticationBindings.METADATA)
+    private metadata: AuthenticationMetadata[],
     @repository(RolMenuRepository)
     private repositorioRolMenu: RolMenuRepository,
-    @inject(AuthenticationBindings.METADATA)
-    private metadata: AuthenticationMetadata[]
-  ) {}
+    @service(AuthService)
+    private servicioAuth: AuthService
+  ) {
 
+  }
 
-
-  /**Autenticacion de un usuario frente a  una accion en la base de datos
-   *
+  /**
+   * Autenticación de un usuario frente a una acción en la base de datos
    * @param request la solicitud con el token
-   * @returns el perfil del usuario,undefined cuando no tiene permiso
+   * @returns el perfil de usuario, undefined cuando no tiene permiso o un httpError
    */
   async authenticate(request: Request): Promise<UserProfile | undefined> {
     let token = parseBearerToken(request);
-
-    if(token){
+    if (token) {
       let idRol = this.servicioSeguridad.obtenerRolDesdeToken(token);
       let idMenu: string = this.metadata[0].options![0];
       let accion: string = this.metadata[0].options![1];
-      let permiso = await this.repositorioRolMenu.findOne({
-        where:{
-          rolId: idRol,
-          menuId: idMenu
-        }
-      })
-      let continuar:boolean=false;
-      if(permiso){
-
-        switch(accion){
-          case "guardar":
-            continuar=permiso.guardar;
-            break;
-            case "editar":
-            continuar=permiso.editar;
-            break;
-
-            case "listar":
-            continuar=permiso.listar;
-            break;
-            case "eliminar":
-              continuar=permiso.eliminar;
-              break;
-
-
-            default:
-              throw new HttpErrors[401]("No es posible ejecutar la accion porque no existe")
-
-        }
-
-        if(continuar){
-          let perfil:UserProfile=Object.assign({
-            permitido:"Ok"
-          });
-          return perfil;
-        }else{
-          return undefined;
-        }
-
-      } else {
-        throw new HttpErrors[401]("No es posible ejecutar la accion por falta de permisos")
+      console.log(this.metadata);
+      try {
+        let res = await this.servicioAuth.VerificarPermisoDeUsuarioPorRol(idRol, idMenu, accion);
+        return res;
+      } catch (e) {
+        throw e;
       }
     }
-
-    throw new HttpErrors[401]("No es posible ejecutar la accion por falta de un token ")
-
+    throw new HttpErrors[401]("No es posible ejecutar la acción por falta de un token.");
   }
 }
